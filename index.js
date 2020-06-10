5a45deb8f1cdb555530350b56c5aed45740cde29
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-06-11 00:04:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -337,11 +337,11 @@ bot.on('message', function(message){
             // for(role in message.guild.roles.cache){
             //     answer += "\n"+role.name+" : `"+role.id+"`";
             // }
-            message.channel.send("->"+message.guild.roles);
+            message.channel.send("->"+message.guild.roles.cache);
         }
         else if(message.content.includes('emoji')){
             var answer = "Debug menu for emojis:";
-            message.channel.send("->"+message.guild.emojis);
+            message.channel.send("->"+message.guild.emojis.cache);
             // for (emoji in message.guild.emojis.cache){
             //     answer += "\n<"+emoji.name+":"+emoji.id+"> -> "+emoji.name+" : `"+emoji.id+"`";
             // }
