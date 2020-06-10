9aa144da42dd481c427eb98b00bee8f853ac245d
AzodFR <40648473+AzodFR@users.noreply.github.com>
2020-06-10 04:18:36 +0200

    add generateor for the candidature in html

--- index.js
+++ index.js
@@ -341,16 +341,16 @@ bot.on('message', function(message){
         }
     }
     else if(message.channel.parentID === '719313575193084025'){
-        if(message.content.startsWith('/cv')){
+        if(message.content.startsWith('/cv ')){
             const args = message.content.slice(4).split("¤");
 
             const embed = new Discord.MessageEmbed()
                 .setAuthor(message.author.username, message.author.avatarURL())
                 .setTitle("Candidature de: "+message.author.username)
-                .addField("Présentation IRL:", args[0].replace('$_$', '\n'))
-                .addField("Présentation in-game:", args[1].replace('$_$', '\n'))
-                .addField("Poste demandé:", args[2].replace('$_$', '\n'))
-                .addField("Motivation:", args[3].replace('$_$', '\n'))
+                .addField("Présentation IRL:", args[0].replace('$^$', '\n'))
+                .addField("Présentation in-game:", args[1].replace('$^$', '\n'))
+                .addField("Poste demandé:", args[2].replace('$^$', '\n'))
+                .addField("Motivation:", args[3].replace('$^$', '\n'))
                 .setFooter("Candidature générer depuis breakerland.fr")
                 .setColor("RANDOM");
             message.channel.send(embed).then(m => {
